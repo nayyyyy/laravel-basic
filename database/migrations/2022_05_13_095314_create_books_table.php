8e5ee263edd5eb551f1cc9e198f2d76debe6b4f5
Nayaka Al Syahreal K <traitorget@gmail.com>
2022-05-13 19:57:24 +0700

    Initial Commit
    First Commit

---
+++ database/migrations/2022_05_13_095314_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('pengarang');
+            $table->string('penerbit');
+            $table->char('tahun_terbit', 4);
+            $table->integer('jml_hal');
+            $table->integer('jml_buku');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,5 @@
+@extends('books.layout')
+
+@section('hello')
+    <h1>Hello World!</h1>
+@endsection
